781aa64c85883767546f676134d57279cc250054
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-01 02:54:54 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(sentence)
+  reversed=[]
+  sentence.split.each do |word|
+    reversed<<"#{word.reverse}"
+  end
+  reversed.join(" ")
+end
+
+def reverse_each_word(sentence)
+  reversed=[]
+  sentence.split.collect do |word|
+    reversed<<"#{word.reverse}"
+  end
+  reversed.join(" ")
+end
