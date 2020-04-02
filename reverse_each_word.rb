3f82441d1be78e6612e02cd442eb706a7fdabb83
Dina Kendle <dina.kendle@gmail.com>
2020-04-02 03:22:03 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  reversed_word = Array.new()
+  sentence.split.each do |word|
+    reversed_word << "#{word.reverse}"
+    
+  end
+    reversed_word.join(" ")
+end
+
