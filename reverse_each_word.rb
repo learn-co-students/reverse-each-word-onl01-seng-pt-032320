b81a88681a0d9decc4077d97029419af739f0820
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-03 23:38:30 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split(" ")
   new_array = []
-  new_array = new_sentence.collect {|word| word.reverse }
+  new_array << new_sentence.collect {|word| word.reverse }
   new_array.join(" ")
 end
